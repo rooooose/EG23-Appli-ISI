60f38b3eee64b8897720d259fc2d5d4fc5683913
Shir Frank <57755204+rooooose@users.noreply.github.com>
2021-05-09 23:43:40 +0200

    Update PageAccueil.java

--- src/sousFenetres/PageAccueil.java
+++ src/sousFenetres/PageAccueil.java
@@ -3,6 +3,8 @@ package sousFenetres;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
@@ -10,6 +12,7 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -19,6 +22,10 @@ import javax.swing.border.EmptyBorder;
 
 import fenetres.NavigationWindow;
 import java.awt.SystemColor;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.UIManager;
+import java.awt.FlowLayout;
 
 public class PageAccueil extends JPanel {
 
@@ -47,7 +54,7 @@ public class PageAccueil extends JPanel {
 		setLayout(new CardLayout(0, 0));
 		
 		JPanel contentPane = new JPanel();
-		contentPane.setBackground(SystemColor.info);
+		contentPane.setBackground(UIManager.getColor("Button.background"));
 		add(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
@@ -65,23 +72,70 @@ public class PageAccueil extends JPanel {
 		logoPanel.setBackground(Color.WHITE);
 		logoPanel.add(new JLabel(new ImageIcon(getClass().getClassLoader().getResource("img/logo_UTT_petit.png"))));
 		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setForeground(Color.BLACK);
+		comboBox.setBackground(Color.WHITE);
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"ISI", "RT", "MTE", "GI", "A2I", "GM"}));
+		comboBox.setSelectedIndex(0);
+		comboBox.setPreferredSize(new Dimension(100,30));
+		comboBox.setSize(comboBox.getPreferredSize());
+		((JLabel)comboBox.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+//		comboBox.getEditor().getEditorComponent().setBackground(Color.YELLOW);
+	
+		
+		
 		GroupLayout gl_topPanel = new GroupLayout(topPanel);
 		gl_topPanel.setHorizontalGroup(
 			gl_topPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_topPanel.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(logoPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED))
+					.addPreferredGap(ComponentPlacement.RELATED, 175, Short.MAX_VALUE)
+					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
 		gl_topPanel.setVerticalGroup(
 			gl_topPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_topPanel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_topPanel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(logoPanel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE))
+					.addGroup(gl_topPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_topPanel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(logoPanel, GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE))
+						.addGroup(gl_topPanel.createSequentialGroup()
+							.addGap(40)
+							.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
 		);
 		topPanel.setLayout(gl_topPanel);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 100));
+		
+		JButton btnSemestre = new JButton("");
+		btnSemestre.setIcon(new ImageIcon(PageAccueil.class.getResource("/img/Fichier 9.png")));
+		btnSemestre.setPreferredSize(new Dimension(300,200));
+		panel.add(btnSemestre);
+		
+		JButton btnEnseignants = new JButton("");
+		btnEnseignants.setIcon(new ImageIcon(PageAccueil.class.getResource("/img/Fichier 8.png")));
+		btnEnseignants.setPreferredSize(new Dimension(300,200));
+		panel.add(btnEnseignants);
+		
+		JButton btnUE = new JButton("");
+		btnUE.setIcon(new ImageIcon(PageAccueil.class.getResource("/img/Fichier 7.png")));
+		btnUE.setPreferredSize(new Dimension(300,200));
+		panel.add(btnUE);
+		
+		JButton btnRepartition = new JButton("");
+		btnRepartition.setIcon(new ImageIcon(PageAccueil.class.getResource("/img/Fichier 6.png")));
+		btnRepartition.setPreferredSize(new Dimension(300,200));
+		panel.add(btnRepartition);
+		
+		JButton btnES = new JButton("");
+		btnES.setIcon(new ImageIcon(PageAccueil.class.getResource("/img/Fichier 3.png")));
+		btnES.setPreferredSize(new Dimension(300,200));
+		panel.add(btnES);
 	}
-
 }
