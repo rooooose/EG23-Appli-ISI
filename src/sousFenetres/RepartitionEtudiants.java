9296140bc450eee7d39c6de495e0ee8ac270b0e0
Gatafracta <71018828+Gatafracta@users.noreply.github.com>
2021-05-17 00:29:00 +0200

    Réparition des étudiants - v1
    Répartition des étudiants dans le parcours :
    - création d'une première version des graphiques "camemberts "
    - mise en place du lien entre le slider est l'image

--- src/sousFenetres/RepartitionEtudiants.java
+++ src/sousFenetres/RepartitionEtudiants.java
@@ -1,14 +1,16 @@
 package sousFenetres;
 
-import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class RepartitionEtudiants extends JPanel {
 
@@ -39,12 +41,34 @@ public class RepartitionEtudiants extends JPanel {
 		JPanel contentPane = new JPanel();
 		contentPane.setBackground(new Color(106, 90, 205));
 		add(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("REPARTITION DES ETUDIANTS DANS LE PARCOURS");
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblNewLabel);
+		JLabel lblImg = new JLabel("REPARTITION DES ETUDIANTS DANS LE PARCOURS");
+		lblImg.setBounds(10, 55, 499, 521);
+		lblImg.setIcon(new ImageIcon(RepartitionEtudiants.class.getResource("/img-camembert/blue/cam1.png")));
+		lblImg.setForeground(Color.WHITE);
+		lblImg.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblImg);
+		
+		JSlider slider = new JSlider();
+		slider.setToolTipText("");
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				JSlider source = (JSlider) e.getSource();
+			    if (!source.getValueIsAdjusting()) {
+			    	int tick = source.getValue();
+			    	//System.out.println(tick);
+			    	if (RepartitionEtudiants.class.getResource("/img-camembert/blue/cam"+tick+".png") != null) {
+			    		lblImg.setIcon(new ImageIcon(RepartitionEtudiants.class.getResource("/img-camembert/blue/cam"+tick+".png")));
+					}
+			    }
+			}
+		});
+		slider.setMaximum(5);
+		slider.setSnapToTicks(true);
+		slider.setPaintTicks(true);
+		slider.setMajorTickSpacing(1);
+		slider.setBounds(270, 621, 454, 45);
+		contentPane.add(slider);
 	}
-
 }
