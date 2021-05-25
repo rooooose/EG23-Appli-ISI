325892899ce1b7702540744f55ac40507f05b27e
Shir Frank <57755204+rooooose@users.noreply.github.com>
2021-05-25 12:14:43 +0200

    gt

---
+++ src/elements/ItemEtudiant.java
@@ -0,0 +1,174 @@
+package elements;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.SoftBevelBorder;
+
+public class ItemEtudiant {
+	
+private static List<ItemEtudiant> arrItemEtudiant = new ArrayList<ItemEtudiant>();
+	
+	private String arrival;
+	private String name;
+	private String status;
+	private List<String> arrUE;
+	private ImageIcon photo = new ImageIcon(ItemEtudiant.class.getResource("/icons/menu/white/photo_default_x76.png"));
+	//Choix aléatoire parmis un set de graphique lors du changement de profil
+	
+	public ItemEtudiant(String name, String status) {
+		this.name = name;
+		this.status = status;
+	}
+	
+	public ItemEtudiant(String name, String status, String arrival) {
+		this.name = name;
+		this.status = status;
+		this.arrival = arrival;
+	}
+	
+	public ItemEtudiant(String name, String status, String arrival, List<String> arrUE, ImageIcon photo) {
+		this.name = name;
+		this.status = status;
+		this.arrival = arrival;
+		this.arrUE = arrUE;
+		this.photo = photo;
+	}
+	
+	private static void initItems() {
+		String[] arrLevel = {"ISI3", "ISI4", "ISI1", "ISI2"};
+		String[] arrName = {"Vincent FLorent", "Jean Lassale", "C\u00E9dric Villani", "Rose Tixier", "Martin Luther King", "Rosa Parks", "Youri Gagarine"};
+		
+		for (int i=0;i<arrName.length;i++) {
+			int rand = (int) Math.round(Math.random()*3);
+			ItemEtudiant.addList(new ItemEtudiant(arrName[i], arrLevel[rand]));
+		}
+	}
+	
+	public static void initPanelItems(JPanel panelResult) {
+		if (arrItemEtudiant.isEmpty()) {
+			initItems();
+		}
+		
+		final int itemWidth = 400;
+		final int itemHeight = 90;
+		
+		for (int i=0;i<arrItemEtudiant.size();i++) {
+
+			JPanel panelEtudiant = new JPanel();
+			panelEtudiant.setPreferredSize(new Dimension(itemWidth, itemHeight));
+			panelEtudiant.setBackground(new Color(255, 255, 255));
+			panelEtudiant.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 2));
+			panelEtudiant.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+			panelResult.add(panelEtudiant);
+			
+			JLabel lblPhoto = new JLabel("");
+//			lblPhoto.setBounds(10, 6, 76, 76);
+//			lblPhoto.setPreferredSize(new Dimension(77, 20));
+			lblPhoto.setIcon((arrItemEtudiant.get(i).getPhoto()));
+			panelEtudiant.add(lblPhoto);
+			
+			JLabel lblName = new JLabel(arrItemEtudiant.get(i).getName());
+			lblName.setForeground(new Color(60, 60, 60));
+			lblName.setFont(new Font("Raleway Medium", Font.PLAIN, 18));
+//			lblName.setBounds(97, 18, 233, 30);
+			panelEtudiant.add(lblName);
+			
+//			JLabel lblStatut = new JLabel(arrItemEtudiant.get(i).getLevel());
+//			lblStatut.setForeground(new Color(14, 100, 161));
+//			lblStatut.setFont(new Font("Raleway Medium", Font.PLAIN, 12));
+//			lblStatut.setBounds(97, 47, 233, 22);
+//			panelEtudiant.add(lblStatut);
+			
+//			JPanel tagBorder = new JPanel();
+//			tagBorder.setLayout(null);
+//			tagBorder.setBackground(new Color(60, 60, 60));
+////			tagBorder.setBounds(270, 12, 50, 30);
+//			panelEtudiant.add(tagBorder);
+//			
+//			JPanel tagBkd = new JPanel();
+//			tagBkd.setLayout(null);
+//			tagBkd.setBackground(SystemColor.scrollbar);
+////			tagBkd.setBounds(3, 3, 44, 24);
+//			tagBorder.add(tagBkd);
+			Border blackline = BorderFactory.createCompoundBorder(
+			        BorderFactory.createLineBorder(Color.CYAN), 
+			        BorderFactory.createEmptyBorder(5, 8, 5, 8));
+			
+			JLabel lblLevel = new JLabel(arrItemEtudiant.get(i).getLevel());
+			lblLevel.setHorizontalAlignment(SwingConstants.CENTER);
+			lblLevel.setForeground(new Color(60, 60, 60));
+			lblLevel.setFont(new Font("Raleway Medium", Font.PLAIN, 17));
+			lblLevel.setBorder(blackline);
+//			lblLevel.setBounds(5, -4, 37, 31);
+			panelEtudiant.add(lblLevel);
+			
+//			if (i == 0) {
+//				UEItems.get(i).addMouseListener(new MouseAdapter() {
+//					@Override
+//					public void mouseClicked(MouseEvent e) {
+//						if (Math.round(splitPane.getDividerLocation()*0.1) == Math.round(splitPane.getMaximumDividerLocation()*0.1)) {
+//							splitPane.setDividerLocation(0.32);
+//						} else {
+//							splitPane.setDividerLocation(1.0);
+//						}
+//					}
+//				});
+//			}
+		}
+	}
+	
+	public static void addList(ItemEtudiant item) {
+		arrItemEtudiant.add(item);
+	}
+	public static void removeList(ItemEtudiant item) {
+		arrItemEtudiant.remove(item);
+	}
+	
+	public void setArrival(String arrival) {
+		this.arrival = arrival;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public void setPhoto(String path) {
+		this.photo = new ImageIcon(ItemEtudiant.class.getResource(path));
+	}
+	
+	public String getArrival() {
+		return this.arrival;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getLevel() {
+		return this.status;
+	}
+	public ImageIcon getPhoto() {
+		return this.photo;
+	}
+	
+	public void addUE(String name) {
+		this.arrUE.add(name);
+	}
+	public void removeUE(String name) {
+		this.arrUE.remove(name);
+	}
+
+}
--- src/sousFenetres/SemestreEnCours.java
+++ src/sousFenetres/SemestreEnCours.java
@@ -30,6 +30,8 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import elements.ColorPalette;
+import elements.ItemEnseignant;
+import elements.ItemEtudiant;
 
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
@@ -178,8 +180,8 @@ public class SemestreEnCours extends JPanel {
 		
 		JSplitPane splitPane = new JSplitPane();
 		splitPane.setEnabled(false);
-		splitPane.setResizeWeight(1.0);
-		splitPane.setDividerLocation(0.1);
+		splitPane.setResizeWeight(0.32);
+		splitPane.setDividerLocation(1.0);
 		splitPane.setContinuousLayout(true);
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
@@ -205,6 +207,11 @@ public class SemestreEnCours extends JPanel {
 		
 		
 		panelResult = new JPanel();
+		panelResult.setBackground(ColorPalette.BKD_BLUE_VERY_LIGHT.getColor());
+//		panelResult.setBounds(2, 2, 392, 612);
+		panelResult.setPreferredSize(new Dimension(12, 12));
+		FlowLayout fl_panelResult = new FlowLayout(FlowLayout.CENTER, 10, 10);
+		panelResult.setLayout(fl_panelResult);
 		
 		
 		scrollPane = new JScrollPane();
@@ -213,159 +220,12 @@ public class SemestreEnCours extends JPanel {
 		splitPane.setLeftComponent(scrollPane);
 		scrollPane.setViewportView(panelResult);
 		
+		ItemEtudiant.initPanelItems(panelResult);
+		
 		SemestreProfile panelProfile = new SemestreProfile();
 		panelProfile.setMinimumSize(new Dimension(0, 0));
 		panelProfile.setPreferredSize(new Dimension(0, 0));
 		panelProfile.setBackground(new Color(255, 255, 240));
 		splitPane.setRightComponent(panelProfile);
 	}
-
-//	panel = new JPanel();
-//	panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-//	
-//	panel_2 = new JPanel();
-//	panel.add(panel_2);
-//	panel_2.setLayout(new BoxLayout(panel_2, BoxLayout.Y_AXIS));
-//	
-//	panel_3 = new JPanel();
-//	panel_2.add(panel_3);
-//	GridBagLayout gbl_panel_3 = new GridBagLayout();
-//	gbl_panel_3.columnWidths = new int[]{150, 150, 150, 0};
-//	gbl_panel_3.rowHeights = new int[]{0, 13, 0};
-//	gbl_panel_3.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-//	gbl_panel_3.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-//	panel_3.setLayout(gbl_panel_3);
-//	
-//	lblNewLabel_3 = new JLabel("CS/TM");
-//	GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-//	gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-//	gbc_lblNewLabel_3.gridx = 0;
-//	gbc_lblNewLabel_3.gridy = 0;
-//	panel_3.add(lblNewLabel_3, gbc_lblNewLabel_3);
-//	
-//	lblNewLabel_4 = new JLabel("ME");
-//	GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-//	gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
-//	gbc_lblNewLabel_4.gridx = 1;
-//	gbc_lblNewLabel_4.gridy = 0;
-//	panel_3.add(lblNewLabel_4, gbc_lblNewLabel_4);
-//	
-//	lblNewLabel_5 = new JLabel("CT");
-//	GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-//	gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 0);
-//	gbc_lblNewLabel_5.gridx = 2;
-//	gbc_lblNewLabel_5.gridy = 0;
-//	panel_3.add(lblNewLabel_5, gbc_lblNewLabel_5);
-//	
-//	lblNewLabel = new JLabel("4/12");
-//	lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//	GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-//	gbc_lblNewLabel.fill = GridBagConstraints.BOTH;
-//	gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
-//	gbc_lblNewLabel.gridx = 0;
-//	gbc_lblNewLabel.gridy = 1;
-//	panel_3.add(lblNewLabel, gbc_lblNewLabel);
-//	
-//	lblNewLabel_1 = new JLabel("7/6");
-//	lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-//	GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-//	gbc_lblNewLabel_1.fill = GridBagConstraints.BOTH;
-//	gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
-//	gbc_lblNewLabel_1.gridx = 1;
-//	gbc_lblNewLabel_1.gridy = 1;
-//	panel_3.add(lblNewLabel_1, gbc_lblNewLabel_1);
-//	
-//	lblNewLabel_2 = new JLabel("2/6");
-//	lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-//	GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-//	gbc_lblNewLabel_2.fill = GridBagConstraints.BOTH;
-//	gbc_lblNewLabel_2.gridx = 2;
-//	gbc_lblNewLabel_2.gridy = 1;
-//	panel_3.add(lblNewLabel_2, gbc_lblNewLabel_2);
-//	
-//	panel_4 = new JPanel();
-//	panel_2.add(panel_4);
-//	panel_4.setLayout(new GridLayout(0, 5, 0, 0));
-//	
-//	lblNewLabel_6 = new JLabel("CS");
-//	lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_6);
-//	
-//	lblNewLabel_10 = new JLabel("TM");
-//	lblNewLabel_10.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_10);
-//	
-//	lblNewLabel_11 = new JLabel("ME");
-//	lblNewLabel_11.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_11);
-//	
-//	lblNewLabel_12 = new JLabel("CT");
-//	lblNewLabel_12.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_12);
-//	
-//	lblNewLabel_13 = new JLabel("NPML");
-//	lblNewLabel_13.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_13);
-//	
-//	lblNewLabel_7 = new JLabel("GL02 A");
-//	lblNewLabel_7.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_7);
-//	
-//	label = new JLabel("");
-//	panel_4.add(label);
-//	
-//	lblNewLabel_14 = new JLabel("GE21 B");
-//	lblNewLabel_14.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_14);
-//	
-//	label_1 = new JLabel("");
-//	panel_4.add(label_1);
-//	
-//	lblNewLabel_15 = new JLabel("Valid\u00E9");
-//	lblNewLabel_15.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_15);
-//	
-//	lblNewLabel_8 = new JLabel("IF02 A");
-//	lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_8);
-//	
-//	label_3 = new JLabel("");
-//	panel_4.add(label_3);
-//	
-//	label_4 = new JLabel("");
-//	panel_4.add(label_4);
-//	
-//	label_5 = new JLabel("");
-//	panel_4.add(label_5);
-//	
-//	label_6 = new JLabel("");
-//	panel_4.add(label_6);
-//	
-//	lblNewLabel_9 = new JLabel("LO02 C");
-//	lblNewLabel_9.setHorizontalAlignment(SwingConstants.CENTER);
-//	panel_4.add(lblNewLabel_9);
-//	
-//	label_7 = new JLabel("");
-//	panel_4.add(label_7);
-//	
-//	label_8 = new JLabel("");
-//	panel_4.add(label_8);
-//	
-//	label_9 = new JLabel("");
-//	panel_4.add(label_9);
-//	
-//	label_10 = new JLabel("");
-//	panel_4.add(label_10);
-//	
-//	label_11 = new JLabel("");
-//	panel_4.add(label_11);
-//	
-//	label_12 = new JLabel("");
-//	panel_4.add(label_12);
-//	
-//	label_13 = new JLabel("");
-//	panel_4.add(label_13);
-//	
-//	label_14 = new JLabel("");
-//	panel_4.add(label_14);
 }
